f5e99b7d5dddd9e169e53ef2aec9cf872324d5e0
Manish Singh <manishsinghadhikari44@gmail.com>
2024-01-08 11:28:50 +0530

    Added two files

---
+++ helloworld.tf
@@ -0,0 +1,9 @@
+output hello1 {
+    value="Hello ${var.username}"
+}
+output myPwd {
+    value="My Passwd is "
+}
+output address {
+    value="The address is ${var.address}"
+}
---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable username {}
+variable address {
+    default="india"
+}
